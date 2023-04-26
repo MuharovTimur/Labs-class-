bc032acaf162949a0cfebd5e04ac6c1198edeeb4
Muharov Timur(ris-22-1b) <116500342+MuharovTimur@users.noreply.github.com>
2023-04-26 19:36:48 +0500

    Add files via upload

---
+++ Class_Lab_1/class.h
@@ -0,0 +1,12 @@
+#pragma once
+class Linear
+{
+ private:
+    float first;
+    float second;
+ public:
+    void Init(float,float);
+    void Read();
+    void Show();
+    float function(double);
+};
